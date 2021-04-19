f95a9aabfc244c07a6d39f05218cc930c38ab7bf
CCW <bb840613@gmail.com>
2021-04-18 23:45:19 -0400

    added files

---
+++ InventoryMgmt2/InventoryMgmt/frm_inventory.Designer.cs
@@ -0,0 +1,372 @@
+﻿
+namespace InventoryMgmt
+{
+    partial class frm_inventory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_id = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_remove = new System.Windows.Forms.Button();
+            this.btn_edit = new System.Windows.Forms.Button();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_pattern = new System.Windows.Forms.TextBox();
+            this.txt_size = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.txt_color = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labl_name = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_qty = new System.Windows.Forms.TextBox();
+            this.Cost = new System.Windows.Forms.Label();
+            this.txt_cost = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txt_price = new System.Windows.Forms.TextBox();
+            this.txt_supplier = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.SystemColors.Control;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.label6.Location = new System.Drawing.Point(12, 19);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(413, 56);
+            this.label6.TabIndex = 140;
+            this.label6.Text = "Inventory Details";
+            this.label6.Click += new System.EventHandler(this.label6_Click_1);
+            // 
+            // txt_id
+            // 
+            this.txt_id.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txt_id.Location = new System.Drawing.Point(12, 151);
+            this.txt_id.Multiline = true;
+            this.txt_id.Name = "txt_id";
+            this.txt_id.Size = new System.Drawing.Size(72, 32);
+            this.txt_id.TabIndex = 139;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label5.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.label5.Location = new System.Drawing.Point(12, 125);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 23);
+            this.label5.TabIndex = 138;
+            this.label5.Text = " ID ";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.Indigo;
+            this.button1.Location = new System.Drawing.Point(1075, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(30, 30);
+            this.button1.TabIndex = 137;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(61, 299);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(963, 302);
+            this.dataGridView1.TabIndex = 136;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // btn_remove
+            // 
+            this.btn_remove.BackColor = System.Drawing.Color.Indigo;
+            this.btn_remove.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_remove.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.btn_remove.Location = new System.Drawing.Point(796, 224);
+            this.btn_remove.Name = "btn_remove";
+            this.btn_remove.Size = new System.Drawing.Size(194, 55);
+            this.btn_remove.TabIndex = 135;
+            this.btn_remove.Text = "Remove";
+            this.btn_remove.UseVisualStyleBackColor = false;
+            this.btn_remove.Click += new System.EventHandler(this.btn_remove_Click_1);
+            // 
+            // btn_edit
+            // 
+            this.btn_edit.BackColor = System.Drawing.Color.Indigo;
+            this.btn_edit.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_edit.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.btn_edit.Location = new System.Drawing.Point(460, 223);
+            this.btn_edit.Name = "btn_edit";
+            this.btn_edit.Size = new System.Drawing.Size(194, 55);
+            this.btn_edit.TabIndex = 134;
+            this.btn_edit.Text = "Edit";
+            this.btn_edit.UseVisualStyleBackColor = false;
+            this.btn_edit.Click += new System.EventHandler(this.btn_edit_Click_1);
+            // 
+            // btn_add
+            // 
+            this.btn_add.BackColor = System.Drawing.Color.Indigo;
+            this.btn_add.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_add.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.btn_add.Location = new System.Drawing.Point(90, 224);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(194, 55);
+            this.btn_add.TabIndex = 133;
+            this.btn_add.Text = "Add";
+            this.btn_add.UseVisualStyleBackColor = false;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.label1.Location = new System.Drawing.Point(486, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 23);
+            this.label1.TabIndex = 132;
+            this.label1.Text = "Size";
+            // 
+            // txt_pattern
+            // 
+            this.txt_pattern.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txt_pattern.Location = new System.Drawing.Point(364, 151);
+            this.txt_pattern.Multiline = true;
+            this.txt_pattern.Name = "txt_pattern";
+            this.txt_pattern.Size = new System.Drawing.Size(116, 32);
+            this.txt_pattern.TabIndex = 131;
+            // 
+            // txt_size
+            // 
+            this.txt_size.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txt_size.Location = new System.Drawing.Point(486, 151);
+            this.txt_size.Multiline = true;
+            this.txt_size.Name = "txt_size";
+            this.txt_size.Size = new System.Drawing.Size(93, 32);
+            this.txt_size.TabIndex = 130;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.label2.Location = new System.Drawing.Point(364, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 23);
+            this.label2.TabIndex = 129;
+            this.label2.Text = "Pattern";
+            // 
+            // txt_name
+            // 
+            this.txt_name.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txt_name.Location = new System.Drawing.Point(90, 151);
+            this.txt_name.Multiline = true;
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(158, 32);
+            this.txt_name.TabIndex = 128;
+            // 
+            // txt_color
+            // 
+            this.txt_color.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txt_color.Location = new System.Drawing.Point(254, 151);
+            this.txt_color.Multiline = true;
+            this.txt_color.Name = "txt_color";
+            this.txt_color.Size = new System.Drawing.Size(104, 32);
+            this.txt_color.TabIndex = 127;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label4.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.label4.Location = new System.Drawing.Point(254, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 23);
+            this.label4.TabIndex = 126;
+            this.label4.Text = "Color";
+            // 
+            // labl_name
+            // 
+            this.labl_name.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.labl_name.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.labl_name.Location = new System.Drawing.Point(90, 125);
+            this.labl_name.Name = "labl_name";
+            this.labl_name.Size = new System.Drawing.Size(128, 23);
+            this.labl_name.TabIndex = 125;
+            this.labl_name.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.label3.Location = new System.Drawing.Point(585, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 23);
+            this.label3.TabIndex = 142;
+            this.label3.Text = "Qty";
+            // 
+            // txt_qty
+            // 
+            this.txt_qty.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txt_qty.Location = new System.Drawing.Point(585, 151);
+            this.txt_qty.Multiline = true;
+            this.txt_qty.Name = "txt_qty";
+            this.txt_qty.Size = new System.Drawing.Size(93, 32);
+            this.txt_qty.TabIndex = 141;
+            // 
+            // Cost
+            // 
+            this.Cost.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.Cost.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Cost.Location = new System.Drawing.Point(684, 125);
+            this.Cost.Name = "Cost";
+            this.Cost.Size = new System.Drawing.Size(128, 23);
+            this.Cost.TabIndex = 144;
+            this.Cost.Text = "Cost";
+            // 
+            // txt_cost
+            // 
+            this.txt_cost.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txt_cost.Location = new System.Drawing.Point(684, 151);
+            this.txt_cost.Multiline = true;
+            this.txt_cost.Name = "txt_cost";
+            this.txt_cost.Size = new System.Drawing.Size(93, 32);
+            this.txt_cost.TabIndex = 143;
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label8.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.label8.Location = new System.Drawing.Point(783, 125);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(128, 23);
+            this.label8.TabIndex = 146;
+            this.label8.Text = "Price";
+            // 
+            // txt_price
+            // 
+            this.txt_price.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txt_price.Location = new System.Drawing.Point(783, 151);
+            this.txt_price.Multiline = true;
+            this.txt_price.Name = "txt_price";
+            this.txt_price.Size = new System.Drawing.Size(93, 32);
+            this.txt_price.TabIndex = 145;
+            // 
+            // txt_supplier
+            // 
+            this.txt_supplier.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txt_supplier.Location = new System.Drawing.Point(882, 151);
+            this.txt_supplier.Multiline = true;
+            this.txt_supplier.Name = "txt_supplier";
+            this.txt_supplier.Size = new System.Drawing.Size(215, 32);
+            this.txt_supplier.TabIndex = 148;
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label7.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.label7.Location = new System.Drawing.Point(882, 125);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(128, 23);
+            this.label7.TabIndex = 147;
+            this.label7.Text = "Supplier Name";
+            // 
+            // frm_inventory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1117, 640);
+            this.Controls.Add(this.txt_supplier);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txt_price);
+            this.Controls.Add(this.Cost);
+            this.Controls.Add(this.txt_cost);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_qty);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txt_id);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_remove);
+            this.Controls.Add(this.btn_edit);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_pattern);
+            this.Controls.Add(this.txt_size);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_name);
+            this.Controls.Add(this.txt_color);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.labl_name);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frm_inventory";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frm_inventory";
+            this.Load += new System.EventHandler(this.frm_inventory_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_id;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_remove;
+        private System.Windows.Forms.Button btn_edit;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_pattern;
+        private System.Windows.Forms.TextBox txt_size;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.TextBox txt_color;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labl_name;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_qty;
+        private System.Windows.Forms.Label Cost;
+        private System.Windows.Forms.TextBox txt_cost;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txt_price;
+        private System.Windows.Forms.TextBox txt_supplier;
+        private System.Windows.Forms.Label label7;
+    }
+}
